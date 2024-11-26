79a562f2cc9d8ba08989684d1930d2f281f38fd7
Abdul12majid <yisaabdulmajid@gmail.com>
2024-11-26 13:22:57 +0100

    updated shell

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ echo 'pick a directory name for set-up: '
 read dir_name
 mkdir $dir_name
 cd $dir_name
-git clone https://github.com/Abdul12majid/rely_home.git
-pip install -r requirements.txt
+git clone https://github.com/Abdul12majid/rely_home.git .
+pip install /-r requirements.txt
 echo "set-up done, kindly go into the folder and run the command - 'python main.py' to run the main code or 'python test.py' to test run."
 echo "each code requires an active url to work, one for the job accept(main.py) and another to select day and time(test.py)"
